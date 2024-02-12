f208aaf0bf742de1308a612ff39cf53961530f63
Maria Frih <maria.frih@gmail.com>
2024-02-12 16:50:37 +0100

    adds a filter to the GET user route to be able to search by username or email

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,10 +5,20 @@ const Family = require("../models/Family.model");
 
 // ! ALL ROUTES ARE PREFIXED BY /api/users
 
-// GET all users
+// GET all users with a search filter on username and email
 router.get("/", async (req, res, next) => {
   try {
-    const allUsers = await User.find();
+    const { search } = req.query;
+    let filter = {};
+    if (search) {
+      filter = {
+        $or: [
+          { username: new RegExp(search, "i") },
+          { email: new RegExp(search, "i") },
+        ],
+      };
+    }
+    const allUsers = await User.find(filter);
     res.status(200).json(allUsers);
   } catch (error) {
     next(error);
